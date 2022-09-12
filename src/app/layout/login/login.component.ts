2b0482cc2a3cff5cb6c2a99eee381d52970a127f
unknown <m.a.vahidkhoo@gmail.com>
2022-09-12 20:20:29 +0430

    login bega raft kar nemikone kola avazshod :-)

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChildren, ElementRef } from '@angular/core';
+import { NgForm, FormControl, FormGroup, Validator, FormBuilder, Validators } from '@angular/forms';
 import { LocalService } from '../../local.service';
 import { Router } from '@angular/router';
+import { Observable, fromEvent, merge } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -9,31 +11,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  registerUser(form: NgForm) {
-    console.log(form.value.password);
-    let checkUser = this.localStore.getData(form.value.username);
-    console.log(checkUser)
-    if (form.value.username == '') {
-      alert('Enter username');
-    } else {
-      if (checkUser == null) {
-        setTimeout(() => {
-          alert("This Username not Exist");
-          this.router.navigate(['/register']);
-        }, 100);
-        
-      }else if (form.value.password == checkUser) {
-        this.router.navigate(['/about']);
-      } else {
-        alert('pass not match');
-      }
-    }
+  loginform: FormGroup;
+
+  constructor(private _localStore: LocalService, private router: Router) {
+    this.loginform = new FormGroup({
+      userName: new FormControl('', [Validators.required]),
+      passWord: new FormControl('', [Validators.required])
+    });
 
   }
-
-  constructor(private localStore: LocalService ,private router: Router) { }
-
   ngOnInit(): void {
+    // localStorage.getItem();
   }
-
+  onSubmit() {
+    if (this.loginform.valid) {
+      console.log(this.loginform.value);
+       
+    }
+  }
 }
